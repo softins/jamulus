57207c32bb6a163d7f9418c6893d97eb85dda431
Tony Mountifield <tony@mountifield.org>
2024-03-22 23:52:50 +0000

    Initial skeleton for TCP protocol server

--- src/server.cpp
+++ src/server.cpp
@@ -75,6 +75,7 @@ CServer::CServer ( const int          iNewMaxNumChan,
     bDisableRaw ( bNDisableRaw ),
     bIPv6Available ( false ),
     Socket ( this, iPortNumber, iQosNumber, strServerBindIP, bNDisableIPv6, bIPv6Available ),
+    TcpServer ( this, strServerBindIP, iPortNumber, bNEnableIPv6 ),
     Logging(),
     iFrameCount ( 0 ),
     HighPrecisionTimer ( bNUseDoubleSystemFrameSize ),
@@ -319,6 +320,10 @@ CServer::CServer ( const int          iNewMaxNumChan,
     // start the socket (it is important to start the socket after all
     // initializations and connections)
     Socket.Start();
+    if ( bEnableTcp )
+    {
+        TcpServer.Start();
+    }
 }
 
 template<unsigned int slotId>
